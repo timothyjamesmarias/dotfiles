924357681c43287835f4009d3a5c120123b64677
timothyjamesmarias <timothyjamesmarias@gmail.com>
2023-12-28 16:46:53 -0800

    neotest

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -734,7 +734,23 @@ require("lazy").setup({
 	{
 		"tpope/vim-rails",
 	},
-	{},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"olimorris/neotest-rspec",
+		},
+    config = function ()
+      local neotest = require("neotest")
+      neotest.setup({
+        adapters = {
+          require("neotest-rspec")
+        }
+      })
+    end
+	},
 })
 
 -- options
