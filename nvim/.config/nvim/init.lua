f952253ab0bf6cfc83b19c6dd76e14d0397a71bf
timothyjamesmarias <timothyjamesmarias@gmail.com>
2024-03-01 19:33:57 -0800

    bunch of new stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -207,6 +207,23 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<CR>")
 		end,
 	},
+
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 	{
 		"williamboman/mason.nvim",
 		dependencies = {
---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+
+return {
+  line_height = 1.7,
+  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font('VictorMono Nerd Font Mono', { weight = 'Regular' }),
+  font_size = 14,
+
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+}
