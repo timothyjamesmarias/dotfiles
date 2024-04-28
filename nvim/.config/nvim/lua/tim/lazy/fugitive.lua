63669912ffbb55d93bc5f7e57e75be91f88e9a8b
timothyjamesmarias <timothyjamesmarias@gmail.com>
2024-04-28 14:17:56 -0700

    fugitive keybindings

--- nvim/.config/nvim/lua/tim/lazy/fugitive.lua
+++ nvim/.config/nvim/lua/tim/lazy/fugitive.lua
@@ -1,6 +1,10 @@
 return {
   "tpope/vim-fugitive",
   config = function ()
-    vim.keymap.set("n", "<leader>gg", "", { silent = true })
+    vim.keymap.set("n", "<leader>gg", "<cmd>Git<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>gw", "<cmd>Gwrite<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>gv", "<cmd>Git difftool<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>gm", "<cmd>Git mergetool<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>gs", ":Gclog -S", { silent = true })
   end
 }
