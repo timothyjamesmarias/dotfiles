6233dcfd3f00a9ae223cb903a1a724e866a4a2e9
timothyjamesmarias <timothyjamesmarias@gmail.com>
2024-04-29 16:50:08 -0700

    terafox

--- nvim/.config/nvim/lua/tim/lazy/theme.lua
+++ nvim/.config/nvim/lua/tim/lazy/theme.lua
@@ -1,10 +1,10 @@
 return {
-	"folke/tokyonight.nvim",
+	"EdenEast/nightfox.nvim",
 	dependencies = {
 		"rktjmp/lush.nvim",
 	},
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme tokyonight-night]])
+		vim.cmd([[colorscheme terafox]])
 	end,
 }
