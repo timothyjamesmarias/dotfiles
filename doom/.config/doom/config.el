ff0a92b581a532cdf046921594547f2aa99c8033
Timothy Marias <timothyjamesmarias@gmail.com>
2024-08-17 13:04:03 -0700

    moar updates

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -31,7 +31,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-challenger-deep)
+(setq doom-theme 'doom-outrun-electric)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
