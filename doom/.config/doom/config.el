6d20ea1a86a8c701dd8f37aca83be46ddabea60e
Timothy Marias <timothyjamesmarias@gmail.com>
2024-09-28 09:54:16 -0700

    update theme

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'kaolin-aurora)
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
