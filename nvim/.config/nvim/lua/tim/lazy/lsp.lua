2b5a3e55dd5aa5351d232a9b378c961d8d3b837a
timothyjamesmarias <timothyjamesmarias@gmail.com>
2025-06-29 17:02:32 -0700

    major cleanup

--- nvim/.config/nvim/lua/tim/lazy/lsp.lua
+++ nvim/.config/nvim/lua/tim/lazy/lsp.lua
@@ -1,132 +1,70 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	dependencies = {
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-		"onsails/lspkind-nvim",
-		"nvim-telescope/telescope.nvim",
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  dependencies = {
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "onsails/lspkind-nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lspkind = require("lspkind")
 
-		local on_attach = function()
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover)
-			vim.keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>")
-		end
+    local on_attach = function()
+      local map = vim.keymap.set
+      map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
+      map("n", "<leader>rn", vim.lsp.buf.rename)
+      map("n", "K", vim.lsp.buf.hover)
+      map("n", "<leader>rs", "<cmd>LspRestart<CR>")
+    end
 
-		lspconfig["html"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "html", "eruby", "blade", "templ" },
-		})
-		lspconfig["sqlls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "sql", "mysql", "pgsql" },
-		})
-		lspconfig["clangd"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "c", "cpp" },
-		})
-		lspconfig["lua_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = {
-							"vim",
-							"require",
-						},
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		})
-		lspconfig["ts_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = {
-				"javascript",
-				"typescript",
-				"html",
-			},
-		})
-		lspconfig["cssls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = {
-				"html",
-				"css",
-			},
-		})
-		lspconfig["intelephense"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		lspconfig["rust_analyzer"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		lspconfig["gopls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		lspconfig["ruby_lsp"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		cmp.setup({
-			formatting = {
-				format = lspkind.cmp_format(),
-			},
-			mapping = {
-				["<C-n>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<C-p>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lsp_signature_help" },
-				{ name = "buffer" },
-				{ name = "path" },
-			},
-			experimental = {
-				native_menu = false,
-				ghost_text = true,
-			},
-		})
-	end,
+    local function lsp_setup(server, opts)
+      lspconfig[server].setup(vim.tbl_deep_extend("force", {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }, opts or {}))
+    end
+
+    -- LSP servers
+    lsp_setup("html", { filetypes = { "html", "eruby", "blade", "templ" } })
+    lsp_setup("cssls", { filetypes = { "html", "css", "scss" } })
+    lsp_setup("ts_ls", { filetypes = { "javascript", "typescript", "html" } })
+    lsp_setup("lua_ls", {
+      settings = {
+        Lua = {
+          runtime = { version = "LuaJIT" },
+          diagnostics = { globals = { "vim", "require" } },
+          workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+          telemetry = { enable = false },
+        },
+      },
+    })
+    lsp_setup("clangd", { filetypes = { "c", "cpp" } })
+    lsp_setup("sqlls", { filetypes = { "sql", "mysql", "pgsql" } })
+    lsp_setup("intelephense") -- PHP
+    lsp_setup("rust_analyzer")
+    lsp_setup("gopls")
+    lsp_setup("ruby_lsp")
+
+    -- Completion config
+    local cmp = require("cmp")
+    cmp.setup({
+      formatting = { format = lspkind.cmp_format() },
+      mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "path" },
+      },
+      experimental = {
+        ghost_text = true,
+      },
+    })
+  end,
 }
