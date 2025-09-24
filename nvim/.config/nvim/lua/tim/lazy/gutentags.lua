e8e29bb78f9e0c92a9bc274ec9a5f07433e84c9e
Timothy Marias <timothy.marias@bythepixel.com>
2025-09-24 09:29:38 -0700

    updates and new slugify script

--- nvim/.config/nvim/lua/tim/lazy/gutentags.lua
+++ nvim/.config/nvim/lua/tim/lazy/gutentags.lua
@@ -1,12 +1,12 @@
 return {
 	"ludovicchabant/vim-gutentags",
 	config = function()
-		vim.g.gutentags_cache_dir = vim.fn.expand("~/.cache/tags")
-		vim.g.gutentags_ctags_extra_args = {
-			"--languages=Ruby,PHP,HTML,CSS,JavaScript,Sass",
-			"--tag-relative=yes",
-			"--fields=+l",
-		}
-		vim.g.gutentags_file_list_command = "rg --files" -- fast file scan
+		-- vim.g.gutentags_cache_dir = vim.fn.expand("~/.cache/tags")
+		-- vim.g.gutentags_ctags_extra_args = {
+		-- 	"--languages=Ruby,PHP,HTML,CSS,JavaScript,Sass",
+		-- 	"--tag-relative=yes",
+		-- 	"--fields=+l",
+		-- }
+		-- vim.g.gutentags_file_list_command = "rg --files" -- fast file scan
 	end,
 }
