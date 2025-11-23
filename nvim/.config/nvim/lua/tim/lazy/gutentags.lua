91ea4922d9e0a5da9535d73d46b25930a7e9d611
Timothy Marias <timothy.marias@bythepixel.com>
2025-11-22 17:50:08 -0800

    refactor(ctags): modernize config to use .ctags.d directory

--- nvim/.config/nvim/lua/tim/lazy/gutentags.lua
+++ nvim/.config/nvim/lua/tim/lazy/gutentags.lua
@@ -6,23 +6,19 @@ return {
 		vim.g.gutentags_generate_on_new = 1
 		vim.g.gutentags_generate_on_missing = 1
 		vim.g.gutentags_generate_on_write = 1
-		vim.g.gutentags_ctags_extra_args = {
-			"--options=" .. vim.fn.expand("~") .. "/dotfiles/.ctags",
-		}
 		vim.g.gutentags_file_list_command = "rg --files"
 
 		-- Project root markers
-		vim.g.gutentags_project_root = { ".git", "Gemfile", "package.json" }
+		vim.g.gutentags_project_root = { ".git", "Gemfile", "package.json", "composer.json" }
 
-		-- Autocommand to generate tags on first open if tags file doesn't exist
-		vim.api.nvim_create_autocmd("VimEnter", {
-			pattern = "*",
-			callback = function()
-				local tags_file = vim.fn.findfile("tags", ".;")
-				if tags_file == "" then
-					vim.fn.jobstart({ "ctags", "-R", "-f", "tags", "--options=" .. vim.fn.expand("~") .. "/dotfiles/.ctags", "." })
-				end
-			end,
-		})
+		-- Exclude these directories from tag generation
+		vim.g.gutentags_ctags_exclude = {
+			"node_modules",
+			"vendor",
+			".git",
+			"dist",
+			"build",
+			"coverage",
+		}
 	end,
 }
