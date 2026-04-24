aa7d5c884104cba32447f9f985a785750bfec088
timothyjamesmarias <timothyjamesmarias@gmail.com>
2026-04-23 17:11:52 -0700

    update: more macos file commands

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -228,6 +228,36 @@ open that file at the current line."
        :desc "Open repo in browser" "o" #'+tim/gh-browse
        :desc "View PR in browser"   "v" #'+tim/gh-pr-view))
 
+(defun +tim/--macos-require ()
+  "Ensure we're on macOS with the `open' CLI available."
+  (unless (eq system-type 'darwin)
+    (user-error "macOS only"))
+  (unless (executable-find "open")
+    (user-error "`open' CLI not found on PATH")))
+
+(defun +tim/--macos-list-apps ()
+  "Return a list of installed .app bundle names for completion."
+  (let ((dirs '("/Applications" "/System/Applications"
+                "/Applications/Utilities" "/System/Applications/Utilities"))
+        names)
+    (dolist (dir dirs)
+      (when (file-directory-p dir)
+        (dolist (f (directory-files dir nil "\\.app\\'" t))
+          (push (file-name-sans-extension f) names))))
+    (sort (delete-dups names) #'string<)))
+
+(defun +tim/macos-open-with (app)
+  "Open the current file (or dired file at point) with APP via `open -a'."
+  (interactive
+   (list (completing-read "Open with app: " (+tim/--macos-list-apps) nil nil)))
+  (+tim/--macos-require)
+  (+macos-open-with app))
+
+(map! :leader
+      (:prefix "o"
+       :desc "Open in default app" "x" #'+macos/open-in-default-program
+       :desc "Open with app…"      "w" #'+tim/macos-open-with))
+
 (after! writeroom-mode
   (setq +zen-text-scale 0
         writeroom-width 80
